0a3535ebb272d672e695937f06268bba9cefbe92
theWarlock <fecypher@gmail.com>
2022-04-16 20:56:15 +0100

    changed user

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // this will be hidden in an envirnoment variable
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAGOXknTqQuxp4IAQPBZm9h8WPtTH8PSBM",
-  authDomain: "investment-application-9072d.firebaseapp.com",
-  projectId: "investment-application-9072d",
-  storageBucket: "investment-application-9072d.appspot.com",
-  messagingSenderId: "299174630339",
-  appId: "1:299174630339:web:92e7781280e3aff4712376",
+  apiKey: "AIzaSyABOp-WxkK-H_X0dHLik8O5o6Zt85rNyYY",
+  authDomain: "coinstationpro-cedd6.firebaseapp.com",
+  projectId: "coinstationpro-cedd6",
+  storageBucket: "coinstationpro-cedd6.appspot.com",
+  messagingSenderId: "623632024894",
+  appId: "1:623632024894:web:8d4989dfbc7c0931b3d86f",
 };
 
 // Initialize Firebase
