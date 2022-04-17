5b3ace2a3335b048f86f845d2a2e43d66c583a33
theWarlock <fecypher@gmail.com>
2022-04-17 06:16:33 +0100

    chat spport

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/61e9e28ff7cf527e84d33bdc/1fpspq0rq";
+      s1.src = "https://embed.tawk.to/625b33eb7b967b11798b1006/default";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
